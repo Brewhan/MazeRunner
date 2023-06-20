e9ecc7d84ef078df62c7f72e19ab036bfc263594
Andrew Ramsier <andrewramsier@gmail.com>
2023-06-20 15:00:23 +0100

    updated the installer

--- install.bat
+++ install.bat
@@ -1,4 +1,5 @@
 for /f "delims=" %%a in ( 'PowerShell -NoProfile -ExecutionPolicy Bypass -Command "[Environment]::GetFolderPath('MyDocuments')" ') do set docs=%%a
-ROBOCOPY /s .\Libs\libraries %docs%\Arduino
-ROBOCOPY /s .\Libs\hardware %docs%\Arduino
-ROBOCOPY /s .MazeRunnerBroken %docs%\Arduino
+ROBOCOPY /s .\Libs\libraries %docs%\Arduino\libraries
+ROBOCOPY /s .\Libs\hardware %docs%\Arduino\hardware
+ROBOCOPY /s .\MazeRunnerBroken %docs%\Arduino\MazeRunnerBroken
+
